fc5e69bd81c047baf88b3f55c9fac832392ae9a7
Turbiani <leo.turbiani@gmail.com>
2018-04-08 11:37:49 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL',
+}];
+let idCount = links.length;
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    },
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
