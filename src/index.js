7ee2a87fff6b0582068399ad75381a3c463691db
Turbiani <leo.turbiani@gmail.com>
2018-04-08 19:54:41 -0300

    Fazend o link entre GraphQL API Application com a GraphQL API Prisma Database através do Prisma Bindings

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
+const { Prisma } = require('prisma-binding');
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL',
-}];
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
     },
   },
 };
@@ -27,5 +23,14 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/public-oasismare-887/hackernews-node/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
