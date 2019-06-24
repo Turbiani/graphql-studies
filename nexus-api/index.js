830e0798ac10f6d56211b3c4ac8d20f09176dff5
turbiani <leo.turbiani@gmail.com>
2019-06-23 23:14:33 -0300

    modularizing schema

--- nexus-api/index.js
+++ nexus-api/index.js
@@ -1,22 +1,5 @@
-const { queryType, stringArg, makeSchema } = require('nexus');
 const { GraphQLServer } = require('graphql-yoga');
-
-const Query = queryType({
-  definition(t) {
-    t.string('hello', {
-      args: { name: stringArg({ nullable: true }) },
-      resolve: (parent, { name }) => `Hello ${name || 'World'}!`,
-    });
-  },
-});
-
-const schema = makeSchema({
-  types: [Query],
-  outputs: {
-    schema: `${__dirname}/generated/schema.graphql`,
-    typegen: `${__dirname}/generated/typings.ts`,
-  },
-});
+const schema = require('./schema');
 
 const server = new GraphQLServer({
   schema,
